ae3b1308307b8b315207096f3bf1f7d33a3cd46f
Aapeli Kutila <aapeli.kutila@gmail.com>
2024-06-09 21:45:03 +0300

    Create note_on and note_off methods for MidiEngine

--- include/MidiEngine.hpp
+++ include/MidiEngine.hpp
@@ -7,6 +7,9 @@ class MidiEngine {
 public:
 	MidiEngine();
 	~MidiEngine();
+
+	void note_on(int pitch, int velocity);
+	void note_off(int pitch, int velocity);
 private:
 	std::unique_ptr<libremidi::midi_in> m_midi_in;
 };
--- src/MidiEngine.cpp
+++ src/MidiEngine.cpp
@@ -3,24 +3,35 @@
 #include <libremidi/libremidi.hpp>
 
 MidiEngine::MidiEngine() {
-	auto message_callback = [](const libremidi::message& message) {
-		auto nBytes = message.size();
-		std::cout << "[ ";
-		for (auto i = 0U; i < nBytes; i++)
-			std::cout << std::hex << (int)message[i] << std::dec << " ";
-		std::cout << "]";
-		if (nBytes > 0)
-			std::cout << " ; stamp = " << message.timestamp;
-		std::cout << std::endl;
+	auto message_callback = [this](const libremidi::message& message) {
+		if (message.size() > 0) {
+			auto message_type = message[0];
+			if (message_type >= 0x90 && message_type <= 0x9F) {
+				note_on(message[1], message[2]);
+			}
+			else if (message_type >= 0x80 && message_type <= 0x8F) {
+				note_off(message[1], message[2]);
+			}
+		}
 		};
 
 	m_midi_in = std::make_unique<libremidi::midi_in>(libremidi::midi_in{ libremidi::input_configuration{.on_message = message_callback} });
 	
 	libremidi::observer obs;
-	for (const libremidi::input_port& port : obs.get_input_ports()) {
-		m_midi_in->open_port(port);
+	libremidi::input_port port{};
+	for (const libremidi::input_port& p : obs.get_input_ports()) {
+		port = p;
 		break;
 	}
+	m_midi_in->open_port(port);
 }
 
 MidiEngine::~MidiEngine() {}
+
+void MidiEngine::note_on(int pitch, int velocity) {
+	std::cout << "Pressed " << pitch << std::endl;
+}
+
+void MidiEngine::note_off(int pitch, int velocity) {
+	std::cout << "Released " << pitch << std::endl;
+}
