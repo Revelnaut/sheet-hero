4f092559c5a75570d68f3d840d4c9cb4f626ff43
Aapeli Kutila <aapeli.kutila@gmail.com>
2024-06-12 00:52:29 +0300

    Add callback functions for input and output device adding/removing

--- include/MidiEngine.hpp
+++ include/MidiEngine.hpp
@@ -15,8 +15,13 @@ public:
 
 	std::vector<int> get_pressed_notes() const;
 private:
+	std::unique_ptr<libremidi::observer> m_observer;
 	std::unique_ptr<libremidi::midi_in> m_midi_in;
 	std::map<int, bool> m_pressed_note_states{};
 
 	void message_callback(libremidi::message&& message);
+	void input_port_added_callback(const libremidi::input_port& port_id);
+	void input_port_removed_callback(const libremidi::input_port& port_id);
+	void output_port_added_callback(const libremidi::output_port& port_id);
+	void output_port_removed_callback(const libremidi::output_port& port_id);
 };
--- src/MidiEngine.cpp
+++ src/MidiEngine.cpp
@@ -6,23 +6,24 @@
 MidiEngine::MidiEngine() {
 	using std::placeholders::_1;
 
-	libremidi::input_configuration configuration{
+	libremidi::input_configuration input_configuration{
 		.on_message = std::bind(&MidiEngine::message_callback, this, _1)
 	};
 
-	m_midi_in = std::make_unique<libremidi::midi_in>(libremidi::midi_in{ configuration });
+	m_midi_in = std::make_unique<libremidi::midi_in>(libremidi::midi_in{ input_configuration });
 	
 	for (int i = 0; i < 128; ++i) {
 		m_pressed_note_states[i] = false;
 	}
 
-	libremidi::observer obs;
-	libremidi::input_port port{};
-	for (const libremidi::input_port& p : obs.get_input_ports()) {
-		port = p;
-		break;
-	}
-	m_midi_in->open_port(port);
+	libremidi::observer_configuration observer_configuration{
+		.input_added = std::bind(&MidiEngine::input_port_added_callback, this, _1),
+		.input_removed = std::bind(&MidiEngine::input_port_removed_callback, this, _1),
+		.output_added = std::bind(&MidiEngine::output_port_added_callback, this, _1),
+		.output_removed = std::bind(&MidiEngine::output_port_removed_callback, this, _1)
+	};
+	
+	m_observer = std::make_unique<libremidi::observer>(libremidi::observer{observer_configuration});
 }
 
 MidiEngine::~MidiEngine() {}
@@ -57,10 +58,48 @@ void MidiEngine::message_callback(libremidi::message&& message)
 	if (message.size() > 0) {
 		auto message_type = message[0];
 		if (message_type >= 0x90 && message_type <= 0x9F) {
-			note_on(message[1], message[2]);
+			if (message[2] == 0) { // Velocity is 0
+				note_off(message[1], message[2]);
+			}
+			else {
+				note_on(message[1], message[2]);
+			}
 		}
 		else if (message_type >= 0x80 && message_type <= 0x8F) {
 			note_off(message[1], message[2]);
 		}
 	}
+}
+
+static void print_port_info(const libremidi::port_information& port_info) {
+	std::cout << "Port: " << port_info.port << std::endl;
+	std::cout << "Port name: " << port_info.port_name << std::endl;
+	std::cout << "Device name: " << port_info.device_name << std::endl;
+	std::cout << "Display name: " << port_info.display_name << std::endl;
+	std::cout << "Manufacturer: " << port_info.manufacturer << std::endl;
+	std::cout << std::endl;
+}
+
+void MidiEngine::input_port_added_callback(const libremidi::input_port& port_id) {
+	std::cout << "Added input device:" << std::endl;
+	print_port_info(port_id);
+
+	if (m_midi_in->is_port_open() == false) {
+		m_midi_in->open_port(port_id);
+	}
+}
+
+void MidiEngine::input_port_removed_callback(const libremidi::input_port& port_id) {
+	std::cout << "Removed input device:" << std::endl;
+	print_port_info(port_id);
+}
+
+void MidiEngine::output_port_added_callback(const libremidi::output_port& port_id) {
+	std::cout << "Added output device:" << std::endl;
+	print_port_info(port_id);
+}
+
+void MidiEngine::output_port_removed_callback(const libremidi::output_port& port_id) {
+	std::cout << "Removed output device:" << std::endl;
+	print_port_info(port_id);
 }
