a90b396a74bd7bc8ae27aefcb77e2d52a72bc178
Aapeli Kutila <aapeli.kutila@gmail.com>
2024-06-20 02:11:52 +0300

    Generate smaller amount of random accidentals in the demo song generator

--- src/App.cpp
+++ src/App.cpp
@@ -73,12 +73,12 @@ Song App::generate_random_song(int measures, Key key, int tempo) {
 	Song song{};
 	song.set_key(key);
 	song.set_tempo(tempo);
-	song.set_time_signature({4, 4});
+	song.set_time_signature({ 4, 4 });
 
 	for (int m = 0; m < measures; ++m) {
 		GrandMeasure grand_measure;
 
-		Measure & treble = grand_measure.treble_measure;
+		Measure& treble = grand_measure.treble_measure;
 
 		while (treble.free_space_in_eights(song.get_time_signature()) > 0) {
 			Value largest_option{};
@@ -104,7 +104,16 @@ Song App::generate_random_song(int measures, Key key, int tempo) {
 
 			int note_count = Random::get(1, 5);
 			for (int i = 0; i < note_count; ++i) {
-				ng.add_note(Note{ static_cast<PitchClass>(staff_pitches[i]), static_cast<Accidental>(Random::get(0, 2)), Random::get(4, 5)});
+				PitchClass pitch_class{ static_cast<PitchClass>(staff_pitches[i]) };
+				Accidental accidental{ song.get_scale().get_accidental(pitch_class) };
+				
+				// Small chance for a random accidental
+				if (Random::get<bool>(0.05)) {
+					accidental = static_cast<Accidental>(Random::get(0, 2));
+				}
+				
+				int octave = Random::get(4, 5);
+				ng.add_note(Note{ pitch_class, accidental, octave });
 			}
 			treble.add_note_group(ng);
 		}
@@ -113,7 +122,7 @@ Song App::generate_random_song(int measures, Key key, int tempo) {
 
 		while (bass.free_space_in_eights(song.get_time_signature()) > 0) {
 			Value largest_option{};
-			
+
 			if (bass.free_space_in_eights(song.get_time_signature()) == 8) {
 				largest_option = Value::Whole;
 			}
@@ -129,13 +138,22 @@ Song App::generate_random_song(int measures, Key key, int tempo) {
 
 			Value value = static_cast<Value>(Random::get<int>(static_cast<int>(largest_option), static_cast<int>(Value::Eight)));
 			NoteGroup ng{ value };
-			
+
 			std::vector<int> staff_pitches{ 0, 1, 2, 3, 4, 5, 6 };
 			Random::shuffle(staff_pitches);
 
 			int note_count = Random::get(1, 3);
 			for (int i = 0; i < note_count; ++i) {
-				ng.add_note(Note{ static_cast<PitchClass>(staff_pitches[i]), static_cast<Accidental>(Random::get(0, 2)), Random::get(2, 3) });
+				PitchClass pitch_class{ static_cast<PitchClass>(staff_pitches[i]) };
+				Accidental accidental{ song.get_scale().get_accidental(pitch_class) };
+
+				// Small chance for a random accidental
+				if (Random::get<bool>(0.05)) {
+					accidental = static_cast<Accidental>(Random::get(0, 2));
+				}
+
+				int octave = Random::get(2, 3);
+				ng.add_note(Note{ pitch_class, accidental, octave });
 			}
 			bass.add_note_group(ng);
 		}
